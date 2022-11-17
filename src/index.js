f1fd055b8475a28fc224942e80b0d1921fb1fc11
Shubham Karma <shubhamkarma2012@gmail.com>
2022-11-18 01:17:32 +0530

    done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const myFileWriter = async (fileName, fileContent) => {
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
-	fs.readFile(fileName,'utf8')
+	fs.readFile(fileName)
 }
 
 
